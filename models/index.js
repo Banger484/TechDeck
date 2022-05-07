a6bd78376f122862ade67447adf771d80bd67187
Bret Banger <Banger484@gmail.com>
2022-05-06 23:03:58 -0500

    fixed associations and built seed data, seeds are working!

--- models/index.js
+++ models/index.js
@@ -6,18 +6,8 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
 })
 
-User.hasMany('Post', {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-})
-
 Comment.belongsTo(User, {
     foreignKey: 'user_id'
-})
-
-User.hasMany(Comment, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
 })
 
 Post.hasMany(Comment, {
